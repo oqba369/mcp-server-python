b692521819cd6ded904e40ed61f852976d7e6cfc
oqba369 <onnnnop.3@gmail.com>
2026-09-04 16:58:38 +0200

    Update server.py

--- server.py
+++ server.py
@@ -52,7 +52,11 @@ YOUTUBE_REFRESH_TOKEN = os.environ.get(
 # ============================================================
 
 YOUTUBE_SCOPES = [
-    "https://www.googleapis.com/auth/youtube.readonly",
+    "https://www.googleapis.com/auth/youtube",
+    "https://www.googleapis.com/auth/youtube.force-ssl",
+    "https://www.googleapis.com/auth/youtube.upload",
+    "https://www.googleapis.com/auth/yt-analytics.readonly",
+    "https://www.googleapis.com/auth/yt-analytics-monetary.readonly",
 ]
 
 
